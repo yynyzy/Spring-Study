fa85be9cddc64449b2fca5561aa0da337d2ab77d
yanyinuo <1601530253@qq.com>
2023-07-06 21:15:20 +0800

    backend: spring study

--- src/main/java/Study1/service/BeanFactoryService2.java
+++ src/main/java/Study1/service/BeanFactoryService2.java
@@ -3,6 +3,8 @@ package Study1.service;
 import Study1.Dao.BeanFactoryDao;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class BeanFactoryService2{
 
@@ -10,6 +12,17 @@ public class BeanFactoryService2{
     private BeanFactoryDao bean4;
     private List<BeanFactoryDao> beanFactoryDaoList;
     private List<?> beanFactoryDaoList2;
+
+    private Set<?> beanSet;
+    private Map<String, String> beanMap;
+
+    public void setBeanSet(Set<?> beanSet) {
+        this.beanSet = beanSet;
+    }
+
+    public void setBeanMap(Map<String, String> beanMap) {
+        this.beanMap = beanMap;
+    }
 
     public void setBean4(BeanFactoryDao bean4) {
         this.bean4 = bean4;
@@ -32,5 +45,7 @@ public class BeanFactoryService2{
         System.out.println(bean4);
         System.out.println(beanFactoryDaoList);
         System.out.println(beanFactoryDaoList2);
+        System.out.println(beanSet);
+        System.out.println(beanMap);
     }
 }
