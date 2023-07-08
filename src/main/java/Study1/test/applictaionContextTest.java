caa8f0ce20f73886f6b2a15f007c2d60b3ff331a
yanyinuo <1601530253@qq.com>
2023-07-08 17:59:29 +0800

    backend: spring study

--- src/main/java/Study1/test/applictaionContextTest.java
+++ src/main/java/Study1/test/applictaionContextTest.java
@@ -9,6 +9,11 @@ import javax.swing.*;
 
 public class applictaionContextTest {
     public static void main(String[] args) {
+
+        // 指定环境
+//        System.setProperty("spring.profiles.active", "dev");
+
+
 //        Spring 容器触发销毁需要使用 ClassPathXmlApplicationContext 类接受使用close()
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 //        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
@@ -28,8 +33,19 @@ public class applictaionContextTest {
 //        Object beanFactory3= applicationContext.getBean("beanFactory3");
 //        System.out.println(beanFactory3);
 
-        BeanFactoryService2 beanFactoryService2= (BeanFactoryService2)applicationContext.getBean("beanFactoryService2");
-        beanFactoryService2.onShow();
+         // 配置bean （含有基本类型，引用类型，集合类型属性）
+//        BeanFactoryService2 beanFactoryService2= (BeanFactoryService2)applicationContext.getBean("beanFactoryService2");
+//        beanFactoryService2.onShow();
 //        System.out.println(beanFactoryService2);
+
+        // beans 的 profile(可以用于区分 dev 和 testing 不同 profile 的bean设置)
+
+//        BeanFactoryDao profileDevBean= (BeanFactoryDao)applicationContext.getBean("profileDevBean");
+//        System.out.println(profileDevBean);
+
+
+        // import 标签引入其他 xml bean 配置
+        BeanFactoryDao importTestBean= (BeanFactoryDao)applicationContext.getBean("import-test");
+        System.out.println(importTestBean);
     }
 }
